6b4041e1a410da2b85b54f9103a16f7cd307d525
Tariqul911 <zxtariqul911@gmail.com>
2026-03-05 22:35:15 +0600

    added show synonyms using function and manage spinner

--- script/app.js
+++ script/app.js
@@ -3,6 +3,18 @@ const createElements = (arr) => {
     return (htmlElements.join(""));
 };
 
+const manageSpinner = (status) => {
+    if(status === true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add("hidden");
+    }
+    else{
+        document.getElementById("spinner").classList.add("hidden");
+        document.getElementById("word-container").classList.remove("hidden");
+    }
+
+}
+
 const loadLessons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all') // promise of response
     .then(res => res.json()) // promise of json data
@@ -18,7 +30,7 @@ const removeActiveClass = () => {
 }
 
 const loadLevelWord = (id) => {
-    
+    manageSpinner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then((res) => res.json())
@@ -98,6 +110,7 @@ const displayLevelWord =(words) => {
         <h2 class="text-5xl font-semibold">নেক্সট Lesson এ যান</h2>
       </div>
         `;
+        manageSpinner(false);
         return;
     }
 
@@ -134,6 +147,7 @@ const displayLevelWord =(words) => {
         `;
         wordContainer.append(card);
     });
+    manageSpinner(false);
 }
 
 const displayLesson = (lessons) => {
